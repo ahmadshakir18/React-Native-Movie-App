3cab598cdc574cc55620fc8d5b62ccc23c09a38c
ahmadshakir18 <ahmad.shakir18@tintash.com>
2018-04-20 15:02:00 +0500

    more yumz

--- Screens/FavsScreen.js
+++ Screens/FavsScreen.js
@@ -4,6 +4,7 @@ import { TouchableHighlight, AsyncStorage, View, ListView, StyleSheet, Navigator
 import {Tile, Card, ListItem, Button, Text, List} from 'react-native-elements';
 import AnimatedList from 'react-native-animated-list';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Toast, {DURATION} from 'react-native-easy-toast';
 import FavsRow from '../FavsRow'
 
 
@@ -30,6 +31,7 @@ export default class FavsScreen extends React.Component {
     _removeItem(movie) {
         let favs = this.state.favs
         var ind = -1
+        this.refs.toast.show('Movie removed from favorites :(');
         for (index in favs) {
             if (favs[index].id == movie.id) {
                 ind = index
@@ -53,23 +55,31 @@ export default class FavsScreen extends React.Component {
     render() {
         this.getFavsFromStorage()
         favs = this.state.favs
-        if (favs.length > 0) {
         return (
+            
+            <View style={{flex:1, backgroundColor:'#1c1c1c'}}>
+            <View>
+            <Toast
+    ref="toast"
+    position='top'
+    positionValue={200}
+    fadeInDuration={300}
+    fadeOutDuration={1000}/>
+    </View>
+            {favs.length > 0 ?
+            <View style={{flex:1}}>
             <AnimatedList
             animation="scale"
         items={favs}
-        duration={400}
+        duration={700}
         renderRow={(rowData) => this._renderRow(rowData, this.props.navigator)}
         onRemove={() => null}
-        />
+        /></View> : <Text style={{marginTop:100, alignSelf:'center', fontSize:18, fontWeight:'bold', color:'white'}}>No favs to display :(</Text>
+            }
+        </View>
         );
     }
-    else {
-        return (
-        <Text>No favs to show :(</Text>
-        );
-    }
-    }
+    
 
     _openDetails(movie) {
         alert("lol")
