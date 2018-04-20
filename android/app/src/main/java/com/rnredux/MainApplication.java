8efb83eb103acbb96f59e90d1f021ea87171bc10
ahmadshakir18 <ahmad.shakir18@tintash.com>
2018-04-20 16:13:35 +0500

    Removed unused modules from components

--- android/app/src/main/java/com/rnredux/MainApplication.java
+++ android/app/src/main/java/com/rnredux/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnredux;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
             new LinearGradientPackage(),
             new OrientationPackage(),
             new KCKeepAwakePackage(),
